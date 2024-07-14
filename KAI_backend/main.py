d6f293b2163548abbe5ee0092847795f7bb37137
yip123 <yzc95127@gmail.com>
2024-07-14 18:23:19 +0800

    uncomment chatbot

--- KAI_backend/main.py
+++ KAI_backend/main.py
@@ -41,12 +41,12 @@ class ChatRequest(BaseModel):
     temperature: Union[float, None] = 0.7
     top_p: Union[float, None] = 0.9
 
-# model, tokenizer = ql.run_model()
+model, tokenizer = ql.run_model()
 
-# terminators = [
-#     tokenizer.eos_token_id,
-#     tokenizer.convert_tokens_to_ids("<|eot_id|>")
-# ]
+terminators = [
+    tokenizer.eos_token_id,
+    tokenizer.convert_tokens_to_ids("<|eot_id|>")
+]
 
 img_model = "Xenova/facial_emotions_image_detection"
 def load_img_onnx_model():
@@ -61,48 +61,48 @@ def load_img_onnx_model():
 
 img_session, extractor = load_img_onnx_model()
 
-# def generate_response(formatted_prompt: str, max_new_tokens: int = 1000, do_sample: bool = True, temperature: float = 0.7, top_p: float = 0.9):
-#     streamer = TextIteratorStreamer(tokenizer, skip_prompt=True, skip_special_tokens=True)
-#     encodeds = tokenizer(
-#         formatted_prompt,
-#         return_tensors='pt'
-#     ).input_ids
+def generate_response(formatted_prompt: str, max_new_tokens: int = 1000, do_sample: bool = True, temperature: float = 0.7, top_p: float = 0.9):
+    streamer = TextIteratorStreamer(tokenizer, skip_prompt=True, skip_special_tokens=True)
+    encodeds = tokenizer(
+        formatted_prompt,
+        return_tensors='pt'
+    ).input_ids
 
-#     def generate_and_signal_complete(encodeds, max_new_tokens, do_sample,
-#         temperature=0.7, top_p=0.9):
-#         model.generate(encodeds, streamer=streamer, max_new_tokens=max_new_tokens, do_sample=do_sample,
-#             eos_token_id=terminators, temperature=temperature, top_p=top_p)
+    def generate_and_signal_complete(encodeds, max_new_tokens, do_sample,
+        temperature=0.7, top_p=0.9):
+        model.generate(encodeds, streamer=streamer, max_new_tokens=max_new_tokens, do_sample=do_sample,
+            eos_token_id=terminators, temperature=temperature, top_p=top_p)
 
-#     t1 = Thread(target=generate_and_signal_complete, kwargs=dict(encodeds=encodeds, max_new_tokens=max_new_tokens, do_sample=do_sample,
-#         temperature=temperature, top_p=top_p))
+    t1 = Thread(target=generate_and_signal_complete, kwargs=dict(encodeds=encodeds, max_new_tokens=max_new_tokens, do_sample=do_sample,
+        temperature=temperature, top_p=top_p))
 
-#     t1.start()
-#     partial_text = ""
-#     lstrip_once = False
-#     for new_text in streamer:
-#         partial_text += new_text
-#         if not lstrip_once:
-#             partial_text = partial_text.lstrip()
-#             lstrip_once = True
-#         print(partial_text)
-#         yield partial_text
+    t1.start()
+    partial_text = ""
+    lstrip_once = False
+    for new_text in streamer:
+        partial_text += new_text
+        if not lstrip_once:
+            partial_text = partial_text.lstrip()
+            lstrip_once = True
+        print(partial_text)
+        yield partial_text
 
-# @app.post("/chat/")
-# async def chat(c_request: ChatRequest):
-#     formatted_prompt = c_request.prompt
-#     max_new_tokens = c_request.max_new_tokens
-#     do_sample = c_request.do_sample
-#     temperature = c_request.temperature
-#     top_p = c_request.top_p
-#     if isinstance(formatted_prompt, list):
-#         formatted_prompt = tokenizer.apply_chat_template(
-#             formatted_prompt,
-#             add_generation_prompt=True,
-#             tokenize=False)
-#         formatted_prompt += "<|start_header_id|>assistant<|end_header_id|>"
-#     print(formatted_prompt)
-#     return StreamingResponse(
-#         generate_response(formatted_prompt, max_new_tokens, do_sample, temperature=temperature, top_p=top_p), media_type="text/plain")
+@app.post("/chat/")
+async def chat(c_request: ChatRequest):
+    formatted_prompt = c_request.prompt
+    max_new_tokens = c_request.max_new_tokens
+    do_sample = c_request.do_sample
+    temperature = c_request.temperature
+    top_p = c_request.top_p
+    if isinstance(formatted_prompt, list):
+        formatted_prompt = tokenizer.apply_chat_template(
+            formatted_prompt,
+            add_generation_prompt=True,
+            tokenize=False)
+        formatted_prompt += "<|start_header_id|>assistant<|end_header_id|>"
+    print(formatted_prompt)
+    return StreamingResponse(
+        generate_response(formatted_prompt, max_new_tokens, do_sample, temperature=temperature, top_p=top_p), media_type="text/plain")
 
 @app.post("/feeling/")
 def feeling_from_image(snapimg: UploadFile):
